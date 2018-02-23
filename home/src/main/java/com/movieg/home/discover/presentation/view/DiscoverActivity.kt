a19806c01179d4fd50e5fef3a0b3b88d95fd43d6
Ruiter Matos <ruitercomp@gmail.com>
2018-02-22 22:45:05 -0300

    creating card layout movie list

--- home/src/main/java/com/movieg/home/discover/presentation/view/DiscoverActivity.kt
+++ home/src/main/java/com/movieg/home/discover/presentation/view/DiscoverActivity.kt
@@ -1,7 +1,6 @@
 package com.movieg.home.discover.presentation.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import com.movieg.home.R
 
