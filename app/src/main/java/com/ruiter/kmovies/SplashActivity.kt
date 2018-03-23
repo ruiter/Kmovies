414a55d6182a011819e981d304598d9f46db073e
Ruiter Matos <ruitercomp@gmail.com>
2018-03-22 22:42:52 -0300

    updating - discover card view

--- app/src/main/java/com/ruiter/kmovies/SplashActivity.kt
+++ app/src/main/java/com/ruiter/kmovies/SplashActivity.kt
@@ -5,10 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.movieg.home.discover.presentation.view.DiscoverActivity
 
-/**
- * Notice that we do not have setContentView() for this SplashActivity.
- * View is displaying from the theme and this way it is faster than creating a layout
- */
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
